6572e6849559bdf280783a6a1a058aff9d0ec7a8
Tomosan-dev <66362923+Tomosan-dev@users.noreply.github.com>
2020-06-14 16:59:07 +0900

    Application.java修正

--- physical-development-api/src/main/java/physicaldevelopment/Application.java
+++ physical-development-api/src/main/java/physicaldevelopment/Application.java
@@ -2,12 +2,19 @@ package physicaldevelopment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource(value = { "classpath:/physicaldevelopment/mybatis.xml" })
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	public static void main(String args[]) throws Exception {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 }
