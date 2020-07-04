254f9f1497aa088397b0eb31c88d885aca4dbbcf
Tomohiro Saito <saito@.com>
2020-07-04 16:35:46 +0900

    createDB sqlを作成

---
+++ sql/create_db.sql
@@ -0,0 +1,10 @@
+-- DROP DATABASE physical_development;
+
+CREATE DATABASE physical_development
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Japanese_Japan.932'
+    LC_CTYPE = 'Japanese_Japan.932'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
