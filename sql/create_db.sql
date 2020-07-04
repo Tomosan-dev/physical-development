9e0b62ae72a315f3d9126b6e46d37293143b36a7
Tomohiro Saito <saito@.com>
2020-07-04 16:41:02 +0900

    ロケールの修正

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -4,7 +4,7 @@ CREATE DATABASE physical_development
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'ja_JP.UTF-8'
-    LC_CTYPE = 'ja_JP.UTF-8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
